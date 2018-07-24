28a91c39e0f3e6f1fdb77444a4d3066157d598f1
eagobert <eagobert@hotmail.com>
2018-07-24 13:58:40 -0500

    initial commit

---
+++ nickelback.py
@@ -0,0 +1,16 @@
+#  Define a set that contains tuples. Each tuple should contain two strings:
+
+    #The name of an artist
+    #A song by that artist
+    #Make sure that some of the songs are from the band Nickelback.
+
+song_list_a = set(
+    ('Beyonce', 'Single Ladies'),
+    ('Beyonce', 'Ego'),
+    ('Beyonce', 'Halo'),
+    ('Nickelback', 'How You Remind Me'),
+    ('Nickelback', 'That Power'),
+    ('Nickelback', 'Animals')
+)
+
+#  Using a set comprehension, create a new set that contains all songs that were not performed by Nickelback.
